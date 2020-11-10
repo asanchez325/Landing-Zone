cab732a94fb4a2481409bd1756374c2ff518633d
Jon Sanchez <jonathan.sanchez@windstream.net>
2020-11-10 16:43:43 -0700

    Now Listening on Port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/config");
+const sequelize = require('./MAIN/config/config');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require('./controllers/'));
+app.use(require('./MAIN/controllers/'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
