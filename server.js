f56215b15e751f50d9ced7e2660ca52c7802a97f
Jon Sanchez <jonathan.sanchez@windstream.net>
2020-11-10 20:29:23 -0700

    Trying to merge

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./MAIN/config/config");
+const sequelize = require('./MAIN/config/config');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
