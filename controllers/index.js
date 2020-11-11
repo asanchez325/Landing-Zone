410b0c6034c733b93a90603fbf1b625befbd0a80
Jon Sanchez <jonathan.sanchez@windstream.net>
2020-11-11 06:02:21 -0700

    Up and Running! more changes

--- controllers/index.js
+++ controllers/index.js
@@ -2,16 +2,16 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
-const salesHistoryRoutes = require('./salesHistory-routes.js');
-const newOrderRoutes = require('./newOrder-routes.js');
-const processPaymentsRoutes = require('./processPayments-routes.js');
-const newAdminRoutes = require('./newAdmin-routes.js');
+const HistoryRoutes = require('./passedRental-routes.js');
+const newRentalRoutes = require('./newRental-routes');
+const processOrderRoutes = require('./processOrder-routes.js');
+const newClientRoutes = require('./newClient-routes');
 
 router.use('/', homeRoutes);
-router.use('/salesHistory', salesHistoryRoutes);
-router.use('/newOrder', newOrderRoutes);
-router.use('/processPayments', processPaymentsRoutes);
-router.use('/newAdmin', newAdminRoutes);
+router.use('/History', HistoryRoutes);
+router.use('/newRental', newRentalRoutes);
+router.use('/processOrder', processOrderRoutes);
+router.use('/newClient', newClientRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
